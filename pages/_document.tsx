3a79bd3d5ca520a1f9eccb8640a98d25cf4ffe4e
takusan23 <takumu1200@gmail.com>
2022-01-07 02:22:53 +0900

    favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
