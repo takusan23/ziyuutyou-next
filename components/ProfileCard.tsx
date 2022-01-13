10e37091128cb0c1d7d6eb29ec24304425808821
takusan23 <takumu1200@gmail.com>
2022-01-14 00:55:00 +0900

    画像ぺたっ

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,14 +1,15 @@
 import Typography from "@mui/material/Typography"
 import CardHeader from "@mui/material/CardHeader"
-import CardMedia from "@mui/material/CardMedia"
 import CardContent from "@mui/material/CardContent"
 import NextAvater from "./NextAvater"
 import React, { useEffect, useState } from "react"
 import CardActions from "@mui/material/CardActions"
 import Button from "@mui/material/Button"
-import BookOutlined from "@mui/icons-material/BookOutlined" 
+import BookOutlined from "@mui/icons-material/BookOutlined"
 import Link from "next/link"
 import RoundedCornerBox from "./RoundedCorner"
+import Image from "next/image"
+import profileCardJpegImg from "../public/profile_card_img.jpg"
 
 /** ProfileCard へ渡すデータ */
 type ProfileCardProps = {
@@ -28,37 +29,42 @@ const ProfileCard: React.FC<ProfileCardProps> = (props) => {
     }, [])
 
     return (
-        <RoundedCornerBox value={3}>
-            <CardHeader
-                avatar={
-                    <NextAvater path="/icon.png" />
-                }
-                title="たくさん"
-                subheader="@takusan_23"
-            />
-            <CardMedia
-                component="img"
-                height="194"
-                alt="なんか思いついたら画像を貼る"
-            />
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    {msg}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Link passHref href="/posts/page/1/">
-                    <Button
-                        sx={{ borderRadius: 10, marginLeft: 'auto' }}
-                        variant="contained"
-                        disableElevation
-                        aria-expanded={true}
-                        startIcon={<BookOutlined />}>
-                        記事一覧へ
-                    </Button>
-                </Link>
-            </CardActions>
-        </RoundedCornerBox>
+        <>
+            <RoundedCornerBox value={3}>
+                <CardHeader
+                    avatar={
+                        <NextAvater path="/icon.png" />
+                    }
+                    title="たくさん"
+                    subheader="@takusan_23"
+                />
+                <div style={{ height: '200px', maxHeight: '200px', position: 'relative' }}>
+                    <Image
+                        alt='別になくてもいい画像'
+                        layout='fill'
+                        objectFit='cover'
+                        src={profileCardJpegImg}
+                    />
+                </div>
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        {msg}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Link passHref href="/posts/page/1/">
+                        <Button
+                            sx={{ borderRadius: 10, marginLeft: 'auto' }}
+                            variant="contained"
+                            disableElevation
+                            aria-expanded={true}
+                            startIcon={<BookOutlined />}>
+                            記事一覧へ
+                        </Button>
+                    </Link>
+                </CardActions>
+            </RoundedCornerBox>
+        </>
     )
 }
 
