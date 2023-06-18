a9b0e00b2d5247ccb5b0d86fb80f1c10a3cefe66
takusan23 <takumu1200@gmail.com>
2023-06-19 02:26:41 +0900

    サイトマップ生成修正

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,24 +2,34 @@ import { MetadataRoute } from "next";
 import UrlTool from "../src/UrlTool";
 import ContentFolderManager from "../src/ContentFolderManager";
 
-/** サイトマップを生成する。Next.js 単体で作れるようになった。 */
+/**
+ * サイトマップを生成する。Next.js 単体で作れるようになった。
+ * Trailing Slash が有効なので最後にスラッシュ入れました。
+ */
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const currentTime = new Date()
 
     // 静的ページ
     const staticPathList: MetadataRoute.Sitemap = [
         {
-            url: UrlTool.BASE_URL,
+            url: `${UrlTool.BASE_URL}/`,
             lastModified: currentTime
         }
     ]
 
-    // ブログ記事と固定ページの名前
-    const [blogPathList, pagePathList] = await Promise.all([ContentFolderManager.getBlogNameList(), ContentFolderManager.getPageNameList()])
-    const dynamicPathList: MetadataRoute.Sitemap = [...blogPathList, ...pagePathList].map(name => ({
-        url: `${UrlTool.BASE_URL}/${name}`,
-        lastModified: currentTime
-    }))
+    // ブログ記事
+    const blogPathList = (await ContentFolderManager.getBlogNameList())
+        .map(name => ({
+            url: `${UrlTool.BASE_URL}/posts/${name}/`,
+            lastModified: currentTime
+        }))
 
-    return [...staticPathList, ...dynamicPathList]
+    // 固定ページ
+    const pagePathList = (await ContentFolderManager.getPageNameList())
+        .map(name => ({
+            url: `${UrlTool.BASE_URL}/pages/${name}/`,
+            lastModified: currentTime
+        }))
+
+    return [...staticPathList, ...blogPathList, ...pagePathList]
 }
