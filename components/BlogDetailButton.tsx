0d0891062a15f73b2dfb50ed962874a48b1a071d
takusan23 <takumu1200@gmail.com>
2023-08-03 02:56:01 +0900

    Twitter シェアを削除

--- components/BlogDetailButton.tsx
+++ components/BlogDetailButton.tsx
@@ -2,28 +2,6 @@ import React from "react"
 import NextLinkButton from "./NextLinkButton"
 import EnvironmentTool from "../src/EnvironmentTool"
 import OpenInBrowserIcon from "../public/icon/open_in_browser.svg"
-import ShareIcon from "../public/icon/share.svg"
-
-/** TwitterShareButton へ渡すデータ */
-type TwitterShareButtonProps = {
-    /** 記事タイトル */
-    title: string
-    /** 記事のURL */
-    url: string
-}
-
-/** Twitterシェアボタン */
-export function TwitterShareButton({ title, url }: TwitterShareButtonProps) {
-    // 共有URL
-    const href = `http://twitter.com/share?url=${encodeURIComponent(url)}&text=${encodeURIComponent(title)}`
-    return (
-        <NextLinkButton
-            variant="text"
-            href={href}
-            startIcon={<ShareIcon />}
-            text="Twitterで共有" />
-    )
-}
 
 /** GitHubHistoryButton へ渡すデータ */
 type GitHubHistoryButtonProps = {
