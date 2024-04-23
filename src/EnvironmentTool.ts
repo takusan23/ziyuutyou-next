3b4fe1c09372fafcbc47d3c80d6c54ebaf584654
takusan23 <takumu1200@gmail.com>
2024-04-24 01:39:59 +0900

    UA を消して、ページ遷移イベントも GA4 の「ブラウザの履歴イベントに基づくページの変更」を使う。

--- src/EnvironmentTool.ts
+++ src/EnvironmentTool.ts
@@ -13,9 +13,6 @@ class EnvironmentTool {
     /** GitHub リポジトリのURL */
     static REPOSITORY_URL = process.env.NEXT_PUBLIC_GITHUB_REPOSITORY_URL!
 
-    /** Google アナリティクス (UA) の 測定ID */
-    static UA_TRACKING_ID = process.env.NEXT_PUBLIC_UA_TRACKING_ID!
-
     /** Google アナリティクス (GA4) の 測定ID */
     static GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID!
 
