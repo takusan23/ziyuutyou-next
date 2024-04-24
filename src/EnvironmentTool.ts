1ddba90956fdd3643c69c49d0f68d6bfa67560a7
takusan23 <takumu1200@gmail.com>
2024-04-25 03:17:58 +0900

    NEXT_PUBLIC_ いらない？

--- src/EnvironmentTool.ts
+++ src/EnvironmentTool.ts
@@ -5,22 +5,22 @@
 class EnvironmentTool {
 
     /** 環境変数からURLを取得する */
-    static BASE_URL = process.env.NEXT_PUBLIC_SITE_BASE_URL!
+    static BASE_URL = process.env.SITE_BASE_URL!
 
     /** サイト名 */
-    static SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME!
+    static SITE_NAME = process.env.SITE_NAME!
 
     /** GitHub リポジトリのURL */
-    static REPOSITORY_URL = process.env.NEXT_PUBLIC_GITHUB_REPOSITORY_URL!
+    static REPOSITORY_URL = process.env.GITHUB_REPOSITORY_URL!
 
     /** Google アナリティクス (GA4) の 測定ID */
-    static GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID!
+    static GA_TRACKING_ID = process.env.GA_TRACKING_ID!
 
     /** Google Search Console の所有権確認のための HTML タグの content の値 */
-    static GOOGLE_SEARCH_CONSOLE = process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE
+    static GOOGLE_SEARCH_CONSOLE = process.env.GOOGLE_SEARCH_CONSOLE
     
     /** 検索 API URL。省略したら undefined */
-    static SEARCH_API_URL = process.env.NEXT_PUBLIC_SEARCH_API_URL
+    static SEARCH_API_URL = process.env.SEARCH_API_URL
 }
 
 export default EnvironmentTool
